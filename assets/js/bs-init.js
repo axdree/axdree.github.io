9c3c6f547464e1b2673e7b32fd34f12cc4341c51
lightcoxa <42603470+lightcoxa@users.noreply.github.com>
2021-04-13 02:10:50 +0800

    updated design & portfolio

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('[data-bs-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
 });
